6e7720a591812347551c0029d9bf9212f8622229
Jonathan Echavarria <jechavarria@reliaquest.com>
2018-07-17 11:23:30 -0400

    added start of pcap search to search route. TODO: need to test this

--- routes/search.rb
+++ routes/search.rb
@@ -122,6 +122,58 @@ post '/search' do
 
     p 'results:' + @results.to_s
 
+  elsif params[:search_type].to_s == 'ssid'
+    @local_results = HVDB.fetch("SELECT a.ssid, p.id, p.plaintext, p.cracked, p.originalhash, p.hashtype, c.name FROM pcaps p LEFT JOIN pcaphashes a on p.id = a.pcap_id LEFT JOIN pcaps f on a.pcap_id = f.id LEFT JOIN customers c on f.customer_id = c.id WHERE a.ssid like '%" + params[:value] + "%'")
+    if @local_results.nil? || @local_results.empty?
+      results_entry['local_cracked'] = '0'
+    else
+      p 'WE HAVE LOCAL ENTRY'
+      p 'LOCAL RESULTS: ' + @local_results.to_s
+      @local_results.each do |local_entry|
+      p 'Local Entry: ' + local_entry.to_s
+      results_entry['id'] = local_entry[:id]
+      results_entry['ssid'] = local_entry[:ssid]
+      results_entry['plaintext'] = local_entry[:plaintext]
+      results_entry['hashtype'] = local_entry[:hashtype]
+      results_entry['originalhash'] = local_entry[:originalhash]
+      results_entry['name'] = local_entry[:name]
+      results_entry['local_cracked'] = '1' if local_entry[:cracked]
+      results_entry['local_cracked'] = '0' unless local_entry[:cracked]
+
+      if hub_settings.status == 'registered' && local_entry[:originalhash]
+        @hash_array = []
+        element = {}
+        element['ciphertext'] = local_entry[:originalhash]
+        element['hashtype'] = local_entry[:hashtype].to_s
+        @hash_array.push(element)
+        hub_response = Hub.hashSearch(@hash_array)
+        hub_response = JSON.parse(hub_response)
+
+        if hub_response['status'] == '200'
+            @hub_hash_results = hub_response['pcaps']
+            @hub_hash_results.each do |entry|
+              results_entry['originalpcap'] = entry['ciphertext'] if entry['cracked'] == '1'
+              results_entry['hashtype'] = entry['hashtype'] if entry['cracked'] == '1'
+              results_entry['show_hub_results'] = '1'
+              results_entry['hub_pcap_id'] = entry['pcap_id']
+              results_entry['hub_cracked'] = '1' if entry['cracked'] == '1'
+              results_entry['hub_cracked'] = '0' if entry['cracked'] == '0' || entry['cracked'].nil?
+            end
+          else
+            flash[:error] = 'Error: Unauthorized access to Hub. Please check settings and try again.'
+          end
+        else
+          results_entry['hub_cracked'] = '0'
+        end
+
+        # We have to push this into an array of 1 because search results page is expecting an array (which is used when searching for usernames or plaintexts)
+        @results.push(results_entry)
+        results_entry = {}
+      end
+    end
+
+    p 'results:' + @results.to_s
+
   elsif params[:search_type] == 'hash'
 
     @local_results = HVDB.fetch("SELECT a.username, h.id, h.plaintext, h.cracked, h.originalhash, h.hashtype, c.name FROM hashes h LEFT JOIN hashfilehashes a on h.id = a.hash_id LEFT JOIN hashfiles f on a.hashfile_id = f.id LEFT JOIN customers c ON f.customer_id = c.id WHERE h.originalhash like '%" + params[:value] + "%'")
