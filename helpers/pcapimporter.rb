0fd318599e1ee4f73bd0b8d9d5fbcac6ddec081b
Jonathan Echavarria <jechavarria@reliaquest.com>
2018-07-17 11:52:52 -0400

    added to pcapimporter helper. TODO: add function to actually handle the importing of a pcap

--- helpers/pcapimporter.rb
+++ helpers/pcapimporter.rb
@@ -1,6 +1,19 @@
 # encoding : utf-8
 
 def ssid(ssid)
-	ssid =~ /\$/ ? true : false
+  ssid =~ /\$/ ? true : false
+end
+
+def getMode(pcap)
+  @modes = []
+  @modes.push('2500') # WPA/WPA2
+end
+
+def modeToFriendly(mode)
+  return 'WPA/WPA2' if mode == '2500'
+end
+
+def friendlyToMode(friendly)
+  return '2500' if friendly = 'WPA/WPA2'
 end
 
